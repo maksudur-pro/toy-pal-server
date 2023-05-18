eb65aabf3c44db14cb39f1a42882944440337417
unknown <ajoy9532@gmail.com>
2023-05-18 14:12:46 +0600

    added dataCollection

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db("toyPal").collection("toys");
+
+    // toys routes
+    app.get("/toys", async (req, res) => {
+      const result = await toyCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
