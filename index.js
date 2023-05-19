e52661a149390ea62ef43e09612b44cb60068230
unknown <ajoy9532@gmail.com>
2023-05-19 15:27:50 +0600

    added myToys server

--- index.js
+++ index.js
@@ -39,6 +39,24 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/toys/:text", async (req, res) => {
+      const result = await toyCollection
+        .find({
+          sub_category: req.params.text,
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/myToys/:email", async (req, res) => {
+      const result = await toyCollection
+        .find({
+          seller_email: req.params.email,
+        })
+        .toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
